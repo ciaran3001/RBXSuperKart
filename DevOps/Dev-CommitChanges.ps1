e3f625ea5d8d051e64d42d2e796be0cbada2dbf9
ciaran300 <ciarankelly15@hotmail.com>
2020-01-14 15:44:10 +0000

    Dev Branch Commit Date: Tuesday 01/14/2020 15:44 +00

--- DevOps/Dev-CommitChanges.ps1
+++ DevOps/Dev-CommitChanges.ps1
@@ -1,5 +1,6 @@
 cd C:\Users\Ciaran\Desktop\Projects\RobloCart\Source
+git checkout Dev
 git add * 
 git commit -m"Dev Branch Commit Date: "$(Get-Date -UFormat "%A %m/%d/%Y %R %Z")
-git push https://github.com/ciaran3001/RBX_SuperKart.git 
+git push https://github.com/ciaran3001/RBX_SuperKart.git Dev
 Read-Host
