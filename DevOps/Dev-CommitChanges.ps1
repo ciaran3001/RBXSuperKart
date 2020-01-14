6e9d4368ad41887e5a72d803e7c262f11c736ed2
ciaran300 <ciarankelly15@hotmail.com>
2020-01-14 15:54:40 +0000

    Automatic Commit Date: Tuesday 01/14/2020 15:54 +00

--- DevOps/Dev-CommitChanges.ps1
+++ DevOps/Dev-CommitChanges.ps1
@@ -1,6 +1,8 @@
 cd C:\Users\Ciaran\Desktop\Projects\RobloCart\Source
-git checkout Dev
+git checkout -b Dev
+git checkout -a
+
 git add * 
 git commit -m"Automatic Commit Date: "$(Get-Date -UFormat "%A %m/%d/%Y %R %Z")
-git push https://github.com/ciaran3001/RBX_SuperKart.git master
+git push https://github.com/ciaran3001/RBX_SuperKart.git Dev:Dev
 Read-Host
