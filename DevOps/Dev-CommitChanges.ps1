441ac7382e181aa158dd65b4b22ca7b766152e8f
ciaran300 <ciarankelly15@hotmail.com>
2020-01-14 15:41:35 +0000

    Dev Branch Commit Date: Tuesday 01/14/2020 15:41 +00

---
+++ DevOps/Dev-CommitChanges.ps1
@@ -0,0 +1,5 @@
+cd C:\Users\Ciaran\Desktop\Projects\RobloCart\Source
+git add * 
+git commit -m"Dev Branch Commit Date: "$(Get-Date -UFormat "%A %m/%d/%Y %R %Z")
+git push https://github.com/ciaran3001/RBX_SuperKart.git Dev
+Read-Host
--- Source/KartController.lua
+++ Source/KartController.lua
@@ -1,47 +1,80 @@
-local InKart = true
+local InKart = true --True for dev
 local Kart = {}
 local turning = true
 local SteerDirection = 0 --1 = Left, 2 = right, 0 = not steering
 local Driving = false
+local Speed = 0
+local Boosted  = false
+local DriveDirection = nil
+local LeftReleased = true
+local RightReleased = true
+local IsFalling = false
+local IsGliding = false
 
 --services
 local UserInputService = game:GetService("UserInputService")
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
+local wheelFL = nil
+local wheelFR = nil
 
+function Initialize()
+	print("Finding car_"..player.Name)
+	local karModel = game.Workspace:WaitForChild("car_"..player.Name)
+	Kart = karModel
+	DriveDirection = Kart.Base.CFrame.lookVector
+	print("Starting Drive Direction: " ..tostring(DriveDirection))
+	wheelFR = Kart.wheelFR 
+	wheelFL = Kart.wheelFL 
+	Speed = 5
+
+end
+function StartDriving()	
+	Driving = true
+	Kart.Base.Velocity = Kart.Base.CFrame.lookVector *50 
+	
+end
+function StopDriving()
+	Driving = false
+end
 function SteerLeft()
 	print("Steer left")
-	local wheelFR = game.Workspace.wheelFR --:FindFirstChild("wheelFR")
-	local wheelFL = game.Workspace.wheelFL --:FindFirstChild("wheelFL")
-	
-	wheelFR.Rotation = Vector3.new(0, -75, 0)
-	wheelFL.Rotation = Vector3.new(0, -75, 0)	
+	LeftReleased = false
+	wheelFL.Rotation = Kart.Base.Rotation + Vector3.new(0, 45, 0)
+	wheelFR.Rotation = Kart.Base.Rotation + Vector3.new(0, 45, 0)
 	turning = true
+	SteerDirection = 1
+
+	wheelFL.Smoke.Enabled = true
+	Kart.wheelBL.Smoke.Enabled = true
 end
 function SteerRight()
 	print("Steer right")
-	local wheelFR = game.Workspace.wheelFR --:FindFirstChild("wheelFR")
-	local wheelFL = game.Workspace.wheelFL --:FindFirstChild("wheelFL"
-
-	wheelFR.Rotation = Vector3.new(0, -105, 0)
-	wheelFL.Rotation = Vector3.new(0, -105, 0)
+	wheelFL.Rotation = Kart.Base.Rotation + Vector3.new(0, -45, 0)	--wheelFL
+	wheelFR.Rotation = Kart.Base.Rotation + Vector3.new(0, -45, 0)   --wheelFR
 	turning = true
-
+	SteerDirection = 2
+	RightReleased = false
+	wheelFR.Smoke.Enabled = true
+	Kart.wheelBR.Smoke.Enabled = true
 end
 function ReleaseSteer()
 	print("Steer Release")
-	local wheelFR = game.Workspace.wheelFR --:FindFirstChild("wheelFR")
-	local wheelFL = game.Workspace.wheelFL --:FindFirstChild("wheelFL")
-
-	wheelFR.Rotation = Vector3.new(0, -90, 0)
-	wheelFL.Rotation = Vector3.new(0, -90, 0)
+	wheelFL.Rotation = Kart.Base.Rotation 
+	wheelFR.Rotation = Kart.Base.Rotation 
 	turning = false 
-end
-
-function ReadInput(inputObject, gameProcessedEvent)
+	SteerDirection = 0
 	
+	wheelFL.Smoke.Enabled = false
+	Kart.wheelBL.Smoke.Enabled = false
+	wheelFR.Smoke.Enabled = false
+	Kart.wheelBR.Smoke.Enabled = false
+end
+function RotateKartParts()
+	local Kart
+end
+function ReadInput(inputObject, gameProcessedEvent)
 	--if gameProcessedEvent then return end   --Is event already actioned by another script?
-
 	-- Next, check that the input was a keyboard event
 	if inputObject.UserInputType == Enum.UserInputType.Keyboard then
 		local input = inputObject.KeyCode.Name
@@ -51,10 +84,18 @@ function ReadInput(inputObject, gameProcessedEvent)
 			if(InKart == true)then
 				SteerLeft()
 			end
-		elseif(input == Enum.KeyCode.D or input == "D")then
+		end
+		if(input == Enum.KeyCode.D or input == "D")then
 			if(InKart == true)then
 				SteerRight()
 			end
+		elseif(input ==Enum.KeyCode.E or input == "E")then
+			if(Driving == false)then
+				StartDriving()
+			else
+				StopDriving()
+			end
+			
 		end
 	end
 end
@@ -66,24 +107,70 @@ function ReleaseInput(inputObject, gameProcessedEvent)
 		if(input ==Enum.KeyCode.A or input == "A")then
 			if(turning == true)then
 				ReleaseSteer()
+				LeftReleased = true
 			end
 		elseif(input == Enum.KeyCode.D or input == "D")then
 			if(turning == true)then
 				ReleaseSteer()
+				RightReleased = true
 			end
 		end
 	end
 end
 
+Initialize()
+UserInputService.InputBegan:Connect(ReadInput)
+UserInputService.InputEnded:Connect(ReleaseInput)
+
+--Main Thread
 while true do
+	if(Driving == true)then
+		print("Driving = true")
+	end
+	
 	wait(.1)
+	if(SteerDirection == 0)then
+		if(LeftReleased == false)then
+			print("Fix left")
+			SteerDirection = 1
+			SteerBugConsolidationCount = SteerBugConsolidationCount+1
+			if(SteerBugConsolidationCount >= 10)then
+				LeftReleased = true
+			end
+		elseif(RightReleased == false)then
+			SteerDirection = 2
+			print("Fix right")
+			SteerBugConsolidationCount = SteerBugConsolidationCount+1
+			if(SteerBugConsolidationCount >= 10)then
+				RightReleased = true
+			end
+		end
+	end
+	
 	if(SteerDirection == 1)then 
-		
+
+		Kart.Base.RotVelocity = Kart.Base.RotVelocity +  Vector3.new(0,.7,0)
+		local KartParts = Kart:GetChildren()
+		print("Starting Drive Direction: " ..tostring(DriveDirection))
+		DriveDirection = Kart.Base.CFrame.lookVector
+
 	elseif(SteerDirection == 2)then
-		
+
+		Kart.Base.RotVelocity = Kart.Base.RotVelocity +  Vector3.new(0,-.7,0)
+		local KartParts = Kart:GetChildren()
+		print("Starting Drive Direction: " ..tostring(DriveDirection))
+		DriveDirection = Kart.Base.CFrame.lookVector
+	else
+		Kart.Base.RotVelocity =  Vector3.new(0,0,0)
+	end
+	
+	if(Driving)then
+		if(Boosted == false)then
+			Kart.Base.Velocity = (DriveDirection *50) + Vector3.new(0,-75,0)
+			print("Drive")
+		end
+	
 	end
 	
 end
 -- Cart event listeners
-UserInputService.InputBegan:Connect(ReadInput)
-UserInputService.InputEnded:Connect(ReleaseInput)
