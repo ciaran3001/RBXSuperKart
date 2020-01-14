2a2f04ceb444027f7be31f6cc3a8d9a2f38d36f9
ciaran300 <ciarankelly15@hotmail.com>
2020-01-14 16:16:02 +0000

    Automatic Dev Commit Date: Tuesday 01/14/2020 16:16 +00

--- DevOps/Dev-CommitChanges.ps1
+++ DevOps/Dev-CommitChanges.ps1
@@ -5,4 +5,4 @@ git branch -a
 git add * 
 git commit -m"Automatic Dev Commit Date: "$(Get-Date -UFormat "%A %m/%d/%Y %R %Z")
 git push https://github.com/ciaran3001/RBX_SuperKart.git Dev:Dev
-Read-Host
+Read-Host "Completed"
